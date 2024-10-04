b52d41f2b7a2cc7896e65d9efbbd7838de8303d9
egotting <henricodss@gmail.com>
2024-10-04 10:58:38 -0300

    Problemas na implementação

--- src/Backend/api-cadastro-backend.Application/UseCases/Usuario/UsuarioGetUseCases.cs
+++ src/Backend/api-cadastro-backend.Application/UseCases/Usuario/UsuarioGetUseCases.cs
@@ -1,10 +1,10 @@
 ﻿using api_cadastro_backend.Domain.Interfaces.Repositories;
 using api_cadastro_backend.Domain.Interfaces.UseCases;
-using api_cadastro_backend.Domain.Models.DTOs.Usuario;
+using api_cadastro_backend.Domain.Models.DTOs;
 
 namespace api_cadastro_backend.Application.UseCases.Usuario;
 
-public class UsuarioGetUseCase : IGettersUseCase
+public class UsuarioGetUseCase //: IUseCaseHandler<GetRequest, UserGetResponseDTO>
 {
     private readonly IUserRepository _repository;
 
@@ -12,20 +12,8 @@ public class UsuarioGetUseCase : IGettersUseCase
     {
         _repository = repository;
     }
-
-
-    public async Task<IEnumerable<Domain.Models.Usuario>> GetAllUsers()
-    {
-        return await _repository.GetAsync();
-    }
-
-    public Task<Domain.Models.Usuario> GetUserById(int id)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<Domain.Models.Usuario> GetUserByEmail(string email)
-    {
-        throw new NotImplementedException();
-    }
+    
+    // public  Task<IEnumerable<UserGetResponseDTO>> Handle(GetRequest request)
+    // {
+    // }
 }
